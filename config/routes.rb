b8355bdfd89b313c9b3ad516987fa72495b0e540
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-06-02 13:22:14 +0200

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#main'
+  root to: 'pages#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :new, :show, :index
 end

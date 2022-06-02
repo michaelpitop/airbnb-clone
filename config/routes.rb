e79198fed3c2058ae785b6de16f12077a9ad821a
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-06-02 13:26:02 +0200

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :new, :show, :index
+  resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
 end

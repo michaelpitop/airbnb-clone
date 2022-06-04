55f10b5559e9bbdbede3140c09d2382bd3eb13ff
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-06-04 15:03:38 +0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#main'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
 end

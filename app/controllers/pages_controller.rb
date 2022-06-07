c0f28d2f1f0237a841af863a3bb8bf906497c498
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-06-07 14:36:45 +0200

    .

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,14 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: :home
 
   def home
+    @listings = Listing.all
+    @users = User.all
   end
 
   def new
   end
 
-  
+  def create
+
+  end
 end
